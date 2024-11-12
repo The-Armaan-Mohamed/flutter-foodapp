4f91c0503bd6ba39a9a1e454ce2bfb6c43c3da7c
Armaan <the.armaan.mohamed@gmail.com>
2024-11-12 20:24:06 +0200

    More UI tweaking

--- lib/pages/register_page.dart
+++ lib/pages/register_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:foodapp/components/my_button.dart';
 import 'package:foodapp/components/my_textfield.dart';
 import 'package:foodapp/services/auth/auth_service.dart';
+import 'package:foodapp/themes/light_mode.dart';
 
 class RegisterPage extends StatefulWidget {
   final void Function()? onTap;
@@ -82,90 +83,120 @@ class _RegisterPageState extends State<RegisterPage> {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return Scaffold(
-      backgroundColor: Theme.of(context).colorScheme.onSurface,
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            // logo
-            Icon(
-              Icons.account_box_rounded,
-              size: 100,
-              color: Theme.of(context).colorScheme.inversePrimary,
+    return MaterialApp(
+      theme: lightMode,
+      home: Scaffold(
+        //backgroundColor: Theme.of(context).colorScheme.onSurface,
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topLeft,
+              end: Alignment(0.8, 1),
+              colors: <Color>[
+                // Color(0xff1f005c),
+                // Color(0xff5b0060),
+                // Color(0xff870160),
+                // Color(0xffac255e),
+                // Color(0xffca485c),
+                // Color(0xffe16b5c),
+                // Color(0xfff39060),
+                // Color(0xffffb56b),
+                Color(0xFFDCDCDC),
+                Color(0xFFBAC4E7),
+                Color(0xFF94AEF1),
+                Color(0xFF6498FA),
+                Color(0xFF009CFF),
+                Color(0xFF00B2EC),
+                Color(0xFF00C1C1),
+                Color(0xFF0ACA91),
+              ],
+              tileMode: TileMode.mirror,
             ),
-
-            const SizedBox(height: 25),
-
-            // Message, app slogan
-            Text(
-              "Let's create an account for you",
-              style: TextStyle(
-                fontSize: 16,
-                color: Theme.of(context).colorScheme.inversePrimary,
-              ),
-            ),
-
-            const SizedBox(height: 25),
-
-            // Email textfield
-            MyTextfield(
-              controller: emailController,
-              hintText: "Email",
-              obscureText: false,
-            ),
-
-            const SizedBox(height: 10),
-
-            // Password textfield
-            MyTextfield(
-              controller: passwordController,
-              hintText: "Password",
-              obscureText: true,
-            ),
-
-            const SizedBox(height: 10),
-
-            // Confirm password textfield
-            MyTextfield(
-              controller: confirmPasswordController,
-              hintText: "Confirm Password",
-              obscureText: true,
-            ),
-
-            const SizedBox(height: 25),
-
-            // Sign up button
-            MyButton(
-              onTap: () {},
-              text: "Sign Up",
-            ),
-
-            const SizedBox(height: 10),
-
-            // Already have an account? Login here
-            Row(
+          ),
+          child: Center(
+            child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Text(
-                  "Already have an account?",
-                  style: TextStyle(
-                      color: Theme.of(context).colorScheme.inversePrimary),
+                // logo
+                Icon(
+                  Icons.account_box_rounded,
+                  size: 100,
+                  color: Theme.of(context).colorScheme.inversePrimary,
                 ),
-                const SizedBox(width: 4),
-                GestureDetector(
-                  onTap: widget.onTap,
-                  child: Text(
-                    "Login now",
-                    style: TextStyle(
-                      color: Theme.of(context).colorScheme.inversePrimary,
-                      fontWeight: FontWeight.bold,
-                    ),
+
+                const SizedBox(height: 25),
+
+                // Message, app slogan
+                Text(
+                  "Let's create an account for you",
+                  style: TextStyle(
+                    fontSize: 16,
+                    color: Theme.of(context).colorScheme.inversePrimary,
                   ),
+                ),
+
+                const SizedBox(height: 25),
+
+                // Email textfield
+                MyTextfield(
+                  controller: emailController,
+                  hintText: "Email",
+                  obscureText: false,
+                ),
+
+                const SizedBox(height: 10),
+
+                // Password textfield
+                MyTextfield(
+                  controller: passwordController,
+                  hintText: "Password",
+                  obscureText: true,
+                ),
+
+                const SizedBox(height: 10),
+
+                // Confirm password textfield
+                MyTextfield(
+                  controller: confirmPasswordController,
+                  hintText: "Confirm Password",
+                  obscureText: true,
+                ),
+
+                const SizedBox(height: 25),
+
+                // Sign up button
+                MyButton(
+                  onTap: () {},
+                  text: "Sign Up",
+                ),
+
+                const SizedBox(height: 10),
+
+                // Already have an account? Login here
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      "Already have an account?",
+                      style: TextStyle(
+                          color: Theme.of(context).colorScheme.inversePrimary),
+                    ),
+                    const SizedBox(width: 4),
+                    GestureDetector(
+                      onTap: widget.onTap,
+                      child: Text(
+                        "Login now",
+                        style: TextStyle(
+                          color: Theme.of(context).colorScheme.inversePrimary,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
               ],
             ),
-          ],
+          ),
         ),
       ),
     );
--- lib/themes/light_mode.dart
+++ lib/themes/light_mode.dart
@@ -2,8 +2,8 @@ import 'package:flutter/material.dart';
 
 ThemeData lightMode = ThemeData(
   colorScheme: ColorScheme.light(
-    onSurface: const Color.fromARGB(1000, 193, 232, 255),
-    surface: Colors.transparent,
+    onSurface: Colors.grey.shade900,
+    //surface: Colors.transparent,
     primary: Colors.grey.shade500,
     secondary: Colors.grey.shade100,
     tertiary: Colors.white,
